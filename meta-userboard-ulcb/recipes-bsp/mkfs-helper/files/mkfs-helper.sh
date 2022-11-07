266e446374447dee5b9e342f3a85a99273b357fe
Lexra Chang <jason.chang@digitalmediacartridge.com>
2022-11-07 15:40:16 +0800

    UUID 971f7d9a-684d-4f90-80f0-9825f95fcb7a

--- meta-userboard-ulcb/recipes-bsp/mkfs-helper/files/mkfs-helper.sh
+++ meta-userboard-ulcb/recipes-bsp/mkfs-helper/files/mkfs-helper.sh
@@ -1,20 +1,25 @@
 #!/bin/sh -e
 
 MACHINE=m3ulcb
-MMCBLK=/dev/mmcblk0
+EMMCBLK=/dev/mmcblk0
+SDMMCBLK=/dev/mmcblk1
 
-[ `mount | grep ' on / type nfs' | wc -l` -eq 0 ] && exit 0
-[ ! -f /boot/core-image-weston-${MACHINE}.tar.gz ] && exit 0
+#[ `mount | grep 'on / ' | grep "${EMMCBLK}" | wc -l` -ne 0 ] && exit 0
+#umount /boot || true
+#mount ${SDMMCBLK}p1 /boot
+[ ! -e /boot/core-image-weston-${MACHINE}.tar.gz ] && exit 0
+[ ! -e /boot/modules-${MACHINE}.tgz ] && exit 0
+[ ! -e /boot/Image ] && exit 0
 
-umount ${MMCBLK}p1 || true
-umount ${MMCBLK}p2 || true
-umount ${MMCBLK}p3 || true
-umount ${MMCBLK}p4 || true
-umount ${MMCBLK}p5 || true
-umount ${MMCBLK}p6 || true
-umount ${MMCBLK}p7 || true
-umount ${MMCBLK}p8 || true
-sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk ${MMCBLK}
+umount ${EMMCBLK}p1 || true
+umount ${EMMCBLK}p2 || true
+umount ${EMMCBLK}p3 || true
+umount ${EMMCBLK}p4 || true
+umount ${EMMCBLK}p5 || true
+umount ${EMMCBLK}p6 || true
+umount ${EMMCBLK}p7 || true
+umount ${EMMCBLK}p8 || true
+sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk ${EMMCBLK}
  d
 
  d
@@ -43,26 +48,27 @@ sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk ${MMCBLK}
 
  t
  1
- c
+ 83
  t
  2
  83
  w
 EOF
 sync
-echo yes | mkfs.vfat -n BOOT ${MMCBLK}p1
-echo yes | mkfs.ext4 -E lazy_itable_init=1,lazy_journal_init=1 ${MMCBLK}p2 -L rootfs -U 614e0000-0000-4b53-8000-1d28000054a9 -jDv
+echo yes | mkfs.ext4 -E lazy_itable_init=0,lazy_journal_init=1 ${EMMCBLK}p1 -L boot -U 971f7d9a-684d-4f90-80f0-9825f95fcb7a -jDv
+echo yes | mkfs.ext4 -E lazy_itable_init=1,lazy_journal_init=1 ${EMMCBLK}p2 -L root -U 62760af2-eac6-4a7b-8b39-ce78365c7d82 -jDv
 
-mount ${MMCBLK}p2 /mnt
+mount ${EMMCBLK}p2 /mnt
 rm -rfv /mnt/*
 tar zxvf /boot/core-image-weston-${MACHINE}.tar.gz -C /mnt
 tar zxvf /boot/modules-${MACHINE}.tgz -C /mnt
-
 umount /mnt
-mount ${MMCBLK}p1 /mnt
+
+mount ${EMMCBLK}p1 /mnt
 rm -rfv /mnt/*
-/bin/cp -fv /boot/Image /mnt
-/bin/cp -fv /boot/*.dtb /mnt
+/bin/cp -Rpfv /boot/Image* /mnt
+/bin/cp -Rpfv /boot/*.dtb /mnt
+umount /boot
 umount /mnt
 
 reboot
--- meta-userboard-ulcb/recipes-bsp/mkfs-helper/mkfs-helper.bb
+++ meta-userboard-ulcb/recipes-bsp/mkfs-helper/mkfs-helper.bb
@@ -5,11 +5,14 @@ SRC_URI = " \
 	file://mkfs-helper.sh \
 "
 
+PACKAGE_ARCH = "${MACHINE_ARCH}"
+
 S = "${WORKDIR}"
 
 do_install () {
 	install -d ${D}/home/root
 	install -m 755 ${WORKDIR}/mkfs-helper.sh ${D}/home/root
+	sed "s/MACHINE=m3ulcb/MACHINE=${MACHINE}/" -i ${D}/home/root/mkfs-helper.sh
 }
 
 do_configure[noexec] = "1"
