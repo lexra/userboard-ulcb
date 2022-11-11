e74dfb2895cfee60aef571e072f6ae6f769f0ce4
Lexra Chang <jason.chang@digitalmediacartridge.com>
2022-11-11 11:59:06 +0800

    PART1=2176

--- mk-sdmmc.sh
+++ mk-sdmmc.sh
@@ -10,8 +10,8 @@ SCRIPT_DIR=`pwd`
 WORK=`pwd`/${TARGET_BOARD}
 
 SDDEV=${WORK}/build/tmp/deploy/images/${TARGET_BOARD}/SDMMC.img
-TOTAL=6144
-PART1=1536
+TOTAL=7168
+PART1=2176
 
 function print_boot_example() {
         echo ""
